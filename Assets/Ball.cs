119531cd507bfbff11673ab2816a37203f851847
unknown <odinffs@gmail.com>
2015-03-03 15:36:50 +0100

    fungerende fartøkning på ball

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class Ball : MonoBehaviour
 {
-
+    public int collisioncounter = 0;
+    public float moveSpeed = 7f;
     private Rigidbody2D ballRB;
     private bool isActive;
 
@@ -19,8 +20,21 @@ public class Ball : MonoBehaviour
             transform.parent = null;
             isActive = true;
             ballRB.isKinematic = false;
-            rigidbody2D.velocity = new Vector2(20, 6);
+            rigidbody2D.velocity = new Vector2(moveSpeed, moveSpeed);
         }
-
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        collisioncounter++;
+        if (collisioncounter == 4)
+        {
+            moveSpeed += 5;
+            rigidbody2D.velocity = new Vector2(moveSpeed, moveSpeed);
+        }
+        else if (collisioncounter == 12)
+        {
+            moveSpeed += 5;
+            rigidbody2D.velocity = new Vector2(moveSpeed, moveSpeed);
+        }
     }
 }
