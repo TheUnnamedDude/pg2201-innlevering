64e70103e964e5318db11d17e02780e44731ab58
unknown <odinffs@gmail.com>
2015-03-03 11:30:08 +0100

    sdkfasl

---
+++ Assets/Ball.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    private Rigidbody2D ballRB;
+    private bool isActive;
+	
+    void Awake()
+    {
+        ballRB = GetComponent<Rigidbody2D>();
+    }
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space) && isActive == false)
+        {
+            transform.parent = null;
+            isActive = true;
+            ballRB.isKinematic = false;
+            rigidbody2D.velocity = new Vector2(20, 6);
+        }
+            
+	}
+}
---
+++ Assets/PaddleController.cs
@@ -0,0 +1,22 @@
+﻿   using UnityEngine;
+using System.Collections;
+
+public class PaddleController : MonoBehaviour {
+    public float moveSpeed = 10f;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, -1.45f, 27.5f);
+        transform.position = pos;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            moveSpeed = 50f;
+        else moveSpeed = 10f;
+    }
+}
