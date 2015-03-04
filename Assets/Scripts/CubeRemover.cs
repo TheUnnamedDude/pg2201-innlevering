4a1bb81c48ec117567f7d3956d2f9c2513a0fc71
NicoD-NITH <nicko_9289@hotmail.com>
2015-03-04 20:38:52 +0100

    Particle kjører

--- Assets/Scripts/CubeRemover.cs
+++ Assets/Scripts/CubeRemover.cs
@@ -17,6 +17,7 @@ public class CubeRemover : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+		Instantiate(GameObject.Find("PS"), transform.position, Quaternion.identity);
         GameManager.Instance.Score += pointsWorth;
         GameManager.Instance.CubeRemoved();
         Destroy(gameObject);
