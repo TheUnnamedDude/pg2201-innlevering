27d2fbcd0009ad59a9af905d5e48d9e59da06456
NicoD-NITH <nicko_9289@hotmail.com>
2015-03-04 15:06:05 +0100

    Samme som jeg skrev for 10 sek siden

---
+++ Assets/Scripts/CubeRemover.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class CubeRemover : MonoBehaviour
+{
+    public int pointsWorth = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameManager.Instance.Score += pointsWorth;
+        GameManager.Instance.CubeRemoved();
+        Destroy(gameObject);
+    }
+    
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private const int numberOfCubes = 88;
+
+    public int CubesRemoved
+    {
+        get;
+        set;
+    }
+
+    public int Score
+    {
+        get;
+        set; 
+    }
+
+    private int _lifes = 3;
+    public int Lifes
+    {
+        get { return _lifes; }
+        set { _lifes = value; }
+    }
+
+    public static GameManager Instance { get; private set; }
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = Time.timeScale > 0 ? 0 : 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            ResetLevel();
+        }
+    }
+
+    public void ResetLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void OnDeath()
+    {
+        Lifes--;
+        if (Instance.Lifes <= 0)
+        {
+            // TODO: Game over
+            Debug.Log("GEIM OVR");
+        }
+        else
+        {
+            GameObject.Find("Paddle").GetComponent<PaddleController>().Reset();
+            GameObject.Find("Sphere").GetComponent<Ball>().Reset();
+        }
+        GameObject.Find("Life").GetComponent<TextMesh>().text = String.Format("Life(s): {0}", Lifes);
+    }
+
+    public void CubeRemoved()
+    {
+        CubesRemoved++;
+        GameObject.Find("Score").GetComponent<TextMesh>().text = string.Format("Score: {0}", Score);
+        if (numberOfCubes - CubesRemoved <= 0)
+        {
+            // TODO: Wonnededed!
+            Debug.Log("You wonnededededed");
+        }
+    }
+}
