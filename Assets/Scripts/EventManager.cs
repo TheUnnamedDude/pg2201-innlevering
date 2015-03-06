12b277f72facb4a46ca3f62678b545eb17086b9c
NicoD-NITH <nicko_9289@hotmail.com>
2015-03-06 14:26:40 +0100

    Dokumentasjon + EventManager har mottatt alt fra ButtonAction

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -3,34 +3,41 @@ using System.Collections;
 
 public class EventManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public void MuteMusic(bool value) {
+		GameObject.Find("Canvas").gameObject.GetComponent<AudioSource> ().mute = !value;
+	}
+	public void Volume(float value) {
+		GameObject.Find("Canvas").gameObject.GetComponent<AudioSource> ().volume = value;
+	}
+	public void Exit(){
+		Application.Quit ();
+	}
+	public void Action(int scene){
+		Application.LoadLevel(scene);
+	}
+	public void MuteGameMusic(bool value) {
+		GameObject.Find("EventSystem").gameObject.GetComponent<AudioSource>().mute = !value;
+	}
+	public void MuteEffects(bool value) {
+		GameObject.Find("Sphere").gameObject.GetComponent<AudioSource>().mute = !value;
+	}
+	public void OnContinuePressed()
+	{
+		GameManager.Instance.GUIToggle("Paused");
+	}
+	public void OnQuitPressed()
+	{
+		Application.LoadLevel(0);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	public void OnRestartPressed()
+	{
+		GameManager.Instance.GUIToggle("Paused");
+		GameManager.Instance.ResetLevel();
 	}
-
-    public void OnContinuePressed()
-    {
-        GameManager.Instance.GUIToggle("Paused");
-    }
-
-    public void OnQuitPressed()
-    {
-        Application.LoadLevel(0);
-    }
-
-    public void OnRestartPressed()
-    {
-        GameManager.Instance.GUIToggle("Paused");
-        GameManager.Instance.ResetLevel();
-    }
-
-    public void OnNextLevelPressed()
-    {
-        Application.LoadLevel(2);
-    }
+	
+	public void OnNextLevelPressed()
+	{
+		Application.LoadLevel(2);
+	}
 }
